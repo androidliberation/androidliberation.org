93c92c50ca2cda495e4ecdecbe47e9c0dfada4da
Luna D2 <droidliberation@gmail.com>
2024-07-28 09:41:47 +0200

    Get rid of daisy ui and make the site mobile friendly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/typography")],
   theme: {
     extend: {
       colors: {
-        "alf-dark": "#0f0004",
-        "alf-light": "#fef2f3",
-        "alf-accent": "#d5294d",
+        red: {
+          50: "#fef2f3",
+          100: "#fde6e7",
+          200: "#fbd0d5",
+          300: "#f8a9b2",
+          400: "#f3798a",
+          500: "#e94a63",
+          600: "#d5294d",
+          700: "#b31d3f",
+          800: "#961b3c",
+          900: "#811a39",
+          950: "#48091a",
+        },
       },
       fontFamily: {
         alf: ['"Roboto Mono"', "monospace"],
@@ -16,24 +26,5 @@ module.exports = {
       sans: ["Roboto", "sans-serif"],
       mono: ['"Roboto Mono"', "monospace"],
     },
-  },
-  daisyui: {
-    themes: [
-      {
-        alf: {
-          950: "#fef2f3",
-          900: "#fde6e7",
-          800: "#fbd0d5",
-          700: "#f7aab2",
-          600: "#f27a8a",
-          500: "#ea546c",
-          400: "#d5294d",
-          300: "#b31d3f",
-          200: "#961b3c",
-          100: "#811a39",
-          50: "#48091a",
-        },
-      },
-    ],
   },
 };
